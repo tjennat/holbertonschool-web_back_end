4bc44817ad7583a543fe0faa3d6f2427f69a6d53
tjennat <theo.jennat98@gmail.com>
2024-04-22 11:13:23 +0200

    script thats gonna concatenate  2 strings

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""function that's gonna concatenate 2 strings"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Return the concatenation of str1 and str2."""
+    return str1 + str2
