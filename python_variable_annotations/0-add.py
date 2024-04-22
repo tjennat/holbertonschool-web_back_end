88a6b6c0b4ddb2c43383e3fa12612597465f56c7
tjennat <theo.jennat98@gmail.com>
2024-04-22 11:13:06 +0200

    fixing pep8

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""function add that takes a float a and a float b as arguments and returns their sum as a float."""
+"""function that add 2 float."""
 
 
 def add(a: float, b: float) -> float:
