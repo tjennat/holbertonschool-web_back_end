1ebe5517bdbe6b3bb17a364d71ea147e1e81cd05
tjennat <theo.jennat98@gmail.com>
2024-04-22 13:23:56 +0200

    script that return the floor of the float

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""function that return the floor of the float."""
+import math
+
+
+def floor(n: float) -> int:
+    """Return the floor of the float."""
+    return math.floor(n)
