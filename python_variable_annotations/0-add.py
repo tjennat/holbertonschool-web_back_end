c04f42064e9d5fb7d8e7ee53fcc9d9fedecc45ea
tjennat <theo.jennat98@gmail.com>
2024-04-22 11:02:58 +0200

    script that will return the sum of a and b

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""function add that takes a float a and a float b as arguments and returns their sum as a float."""
+
+
+def add(a: float, b: float) -> float:
+    """Return the sum of a and b."""
+    return a + b
