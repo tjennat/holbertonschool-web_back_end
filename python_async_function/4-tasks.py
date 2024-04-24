af69c2fdc544d22ab25d413590fd542fcf3d592f
tjennat <theo.jennat98@gmail.com>
2024-04-24 21:29:27 +0200

    Fixing pep8 and name of func

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -8,14 +8,14 @@ task_wait_random = __import__('3-tasks').task_wait_random
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """gonna return the list of all the delays."""
-    delay_list = []
+    delay_l = []
     for _ in range(1, n + 1):
-        delay_list.append(await task_wait_random(max_delay))
+        delay_l.append(await task_wait_random(max_delay))
     for i in range(1, n):
-        cle = delay_list[i]
+        cle = delay_l[i]
         j = i - 1
-        while j >= 0 and delay_list[j] > cle:
-            delay_list[j + 1] = delay_list[j]
+        while j >= 0 and delay_l[j] > cle:
+            delay_l[j + 1] = delay_l[j]
             j -= 1
-        delay_list[j + 1] = cle
-    return delay_list
+        delay_l[j + 1] = cle
+    return delay_l
