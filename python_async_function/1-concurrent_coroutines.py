7331559a6db39754a29e0fe6052b68c7fe852142
tjennat <theo.jennat98@gmail.com>
2024-04-24 20:13:15 +0200

    trying something else

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 """asynchronous coroutine that takes in an integer argument"""
 import asyncio
-import random
 from typing import List
 
 
@@ -9,15 +8,6 @@ import wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
-    """returns 'delay' times in asc order."""
-    delay_l = []
-    for _ in range(n):
-        delay_l.append(wait_random(max_delay))
-    time_length = await asyncio.gather(*delays)
-    for i in range(len(time_length)):
-        for j in range(len(time_length) - 1):
-            if time_length[j] > time_length[j + 1]:
-                temp = time_length[j]
-                time_length[j] = time_length[j + 1]
-                time_length[j + 1] = temp
-    return time_length
+    """returns list of delays in ascending order"""
+    delays = [wait_random(max_delay) for _ in range(n)]
+    return [await delay for delay in asyncio.as_completed(delays)]
