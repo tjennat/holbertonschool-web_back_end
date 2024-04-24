dfa112613cf4881e2cecdc437f7876c424f2de8e
tjennat <theo.jennat98@gmail.com>
2024-04-24 10:05:07 +0200

    Fixing lst type

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """function that returns the length of a list"""
-from typing import Iterable, Sequence, List, Tuple
+from typing import Iterable, List, Sequence, Tuple
 
 
-def element_length(lst: Sequence) -> List[Tuple[Sequence, int]]:
-    """return a list of tuples, a sequence and its length"""
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """returns values with their length in a list"""
     return [(i, len(i)) for i in lst]
