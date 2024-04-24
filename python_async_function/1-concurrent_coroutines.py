9b6970b411b9fc4dc534b82ee025ae2217b581e1
tjennat <theo.jennat98@gmail.com>
2024-04-24 20:19:55 +0200

    Script that's gonna return list of all delay in asc order

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -1,13 +1,21 @@
 #!/usr/bin/env python3
 """asynchronous coroutine that takes in an integer argument"""
-import asyncio
 from typing import List
 
 
-import wait_random = __import__('0-basic_async_syntax').wait_random
+wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
-    """returns list of delays in ascending order"""
-    delays = [wait_random(max_delay) for _ in range(n)]
-    return [await delay for delay in asyncio.as_completed(delays)]
+    """return list of all delay in asc order"""
+    delay_l = []
+    for _ in range(1, n + 1):
+        delay_l.append(await wait_random(max_delay))
+    for i in range(1, n):
+        cle = delay_l[i]
+        j = i - 1
+        while j >= 0 and delay_l[j] > cle:
+            delay_l[j + 1] = delay_l[j]
+            j -= 1
+        delay_l[j + 1] = cle
+    return delay_l
