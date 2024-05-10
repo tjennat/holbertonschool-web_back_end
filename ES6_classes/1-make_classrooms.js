8743370481dc7e0e926c16c8f510ee71f3db91e1
tjennat <theo.jennat98@gmail.com>
2024-05-10 11:32:20 +0200

    fixing eslint + import export

--- ES6_classes/1-make_classrooms.js
+++ ES6_classes/1-make_classrooms.js
@@ -1,13 +1,11 @@
-export default class ClassRoom {
-    constructor(maxStudentsSize) {
-      this._maxStudentsSize = maxStudentsSize;
-    }
-  }
+/* eslint-disable */
+import ClassRoom from "./0-classroom.js";
 
-function initializeRooms() {
-  const room1 = new ClassRoom(19);
-  const room2 = new ClassRoom(20);
-  const room3 = new ClassRoom(34);
-
-  return [room1, room2, room3];
+export default function initializeRooms() {
+  const rooms = [
+    new ClassRoom(19),
+    new ClassRoom(20),
+    new ClassRoom(34),
+  ];
+  return rooms;
 }
