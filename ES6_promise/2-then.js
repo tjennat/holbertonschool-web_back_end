20bebd58cdb4efb29721ef1612067d57a6d249b4
tjennat <theo.jennat98@gmail.com>
2024-05-16 12:56:19 +0200

    2. Catch me if you can!

---
+++ ES6_promise/2-then.js
@@ -0,0 +1,12 @@
+export default function handleResponseFromAPI(promise) {
+  return promise
+    .then(() => ({
+      status: 200,
+      body: 'success',
+    }))
+    .then((response) => {
+      console.log('Got a response from the API');
+      return response;
+    })
+    .catch(() => new Error());
+}
