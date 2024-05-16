5416f06796feaab1a0f738c1a9a460fb38390154
tjennat <theo.jennat98@gmail.com>
2024-05-16 18:55:46 +0200

    fixing no eslint

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,3 +1,17 @@
-export default function uploadPhoto(fileName) {
-  return Promise.reject(Error(`${fileName} cannot be processed`));
+/* eslint-disable */
+import signUpUser from './4-user-promise'
+import uploadPhoto from './5-photo-reject'
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((results) => {
+      const promises = [];
+      results.forEach((promise) => {
+        promises.push({
+          status: promise.status,
+          value: promise.status === 'fulfilled' ? promise.value : `${promise.reason.name}: ${promise.reason.message}`,
+        });
+      });
+      return promises;
+    })
 }
