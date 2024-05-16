e4d2e2bd3a95979855a483b1f6d269d29405bd28
tjennat <theo.jennat98@gmail.com>
2024-05-16 11:55:31 +0200

    fixing issue

--- ES6_promise/0-promise.js
+++ ES6_promise/0-promise.js
@@ -1,4 +1,6 @@
-import getResponseFromAPI from "./0-promise.js";
-
-const response = getResponseFromAPI();
-console.log(response instanceof Promise);
+export default function getResponseFromAPI() {
+    return new Promise((resolve, reject) => {
+      resolve();
+      reject();
+    });
+  }
